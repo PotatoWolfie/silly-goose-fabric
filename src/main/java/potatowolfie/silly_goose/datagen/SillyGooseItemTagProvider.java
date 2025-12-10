7e75c88952f21677946bfffa1c27c63426696bac
PotatoWolfie <lonewolfwolfy6@gmail.com>
2025-12-10 18:23:41 -0500

    1.0.2 - Tags

--- src/main/java/potatowolfie/silly_goose/datagen/SillyGooseItemTagProvider.java
+++ src/main/java/potatowolfie/silly_goose/datagen/SillyGooseItemTagProvider.java
@@ -2,8 +2,11 @@ package potatowolfie.silly_goose.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.ItemTags;
+import potatowolfie.silly_goose.item.SillyGooseItems;
+import potatowolfie.silly_goose.registry.SillyGooseItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -14,8 +17,13 @@ public class SillyGooseItemTagProvider extends FabricTagProvider.ItemTagProvider
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-        valueLookupBuilder(ItemTags.EGGS)
-                .add();
+        valueLookupBuilder(ItemTags.WOLF_FOOD)
+                .add(SillyGooseItems.RAW_GOOSE)
+                .add(SillyGooseItems.COOKED_GOOSE);
 
+        valueLookupBuilder(SillyGooseItemTags.Item.GOOSE_EGGS)
+                .add(SillyGooseItems.WHITE_EGG)
+                .add(SillyGooseItems.BIG_WHITE_EGG)
+                .add(SillyGooseItems.SMALL_WHITE_EGG);
     }
 }
